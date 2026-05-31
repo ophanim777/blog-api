898427bf2a3311679fab3639b47f94b8ba23a421
ophanim777 <rikyjo49@gmail.com>
2026-05-31 23:24:48 +0700

    add setup express for location path

---
+++ config/prisma.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+
+const app = express();
