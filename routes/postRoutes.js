5c5bc98f564ffdae0ab85072bab2bd062d94e417
ophanim777 <rikyjo49@gmail.com>
2026-05-31 23:51:47 +0700

    add routes and export module

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,3 +9,49 @@ const auth = require(
 const admin = require(
   "../middleware/adminMiddleware"
 );
+
+
+const {
+  getPublishedPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+  togglePublish
+} = require(
+  "../controllers/postController"
+);
+
+router.get("/", getPublishedPosts);
+
+router.get("/:id", getPost);
+
+router.post(
+  "/",
+  auth,
+  admin,
+  createPost
+);
+
+router.put(
+  "/:id",
+  auth,
+  admin,
+  updatePost
+);
+
+router.delete(
+  "/:id",
+  auth,
+  admin,
+  deletePost
+);
+
+router.patch(
+  "/:id/publish",
+  auth,
+  admin,
+  togglePublish
+);
+
+module.exports = router;
