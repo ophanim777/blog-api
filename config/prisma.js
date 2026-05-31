9a8d242181156c2700c3b376c818bd346c53d92d
ophanim777 <rikyjo49@gmail.com>
2026-05-31 23:25:19 +0700

    add app use

--- config/prisma.js
+++ config/prisma.js
@@ -6,3 +6,11 @@ const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
