ae90cfb9000ae83f63e17fbd09ef600dc74c9347
ophanim777 <rikyjo49@gmail.com>
2026-05-31 23:54:08 +0700

    add function to create comments

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -1,1 +1,19 @@
 const prisma = require("../config/prisma");
+
+exports.createComment = async (
+  req,
+  res
+) => {
+  const { content, postId } = req.body;
+
+  const comment =
+    await prisma.comment.create({
+      data: {
+        content,
+        postId,
+        userId: req.user.id
+      }
+    });
+
+  res.status(201).json(comment);
+};
