50cc458a728938d518d2c395cffd4832cbfcac34
ophanim777 <rikyjo49@gmail.com>
2026-05-31 23:33:00 +0700

    add login and jwt authentication

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -34,3 +34,49 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { username }
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const match = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!match) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d"
+      }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
